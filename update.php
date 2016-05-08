196fb38ffa82b4de65adb2da7ef6e71eee362974
Dravid Joseph <dravidjoseph@gmail.com>
2016-05-08 17:01:09 -0500

    Add update.php changes

--- update.php
+++ update.php
@@ -1,6 +1,9 @@
 <?php
-
+session_start();
 include('config.php');
+
+$username = $_SESSION['login_user'];
+$title = $_GET['Title'];
 
 $someVar = $mysqli->query("UPDATE projData SET 'mondayTask1' = 'val' WHERE 'title' = 'val'");
 
