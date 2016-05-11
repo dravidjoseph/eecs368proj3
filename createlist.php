4a35f1339ee88b528f3a231de5b0b55649416b83
Michael <mikewang.6127@gmail.com>
2016-05-11 16:39:59 -0500

    make the adding tasks dynamic

--- createlist.php
+++ createlist.php
@@ -1,176 +1,98 @@
 <?php
+	// created by Michael Wang
 	//these functions get the information from the html page.
-	//created by Mike Wang
+
 	session_start();
-	//get username from login
 	$username = $_SESSION['login_user'];
-	//title of list
-	$title = $_GET['Title'];
+	$title = $_POST['Title'];
+	$counter = $_POST['counter'];
+	$taskCounter=0;
 	
-	//all tasks
-	$mt1 = $_GET['mon1'];
-	$mt2 = $_GET['mon2'];
-	$mt3 = $_GET['mon3'];
-	$tut1 = $_GET['tue1'];
-	$tut2 = $_GET['tue2'];
-	$tut3 = $_GET['tue3'];
-	$wt1 = $_GET['wed1'];
-	$wt2 = $_GET['wed2'];
-	$wt3 = $_GET['wed3'];
-	$tht1 = $_GET['thu1'];
-	$tht2 = $_GET['thu2'];
-	$tht3 = $_GET['thu3'];
-	$ft1 = $_GET['fri1'];
-	$ft2 = $_GET['fri2'];
-	$ft3 = $_GET['fri3'];
-	$sat1 = $_GET['sat1'];
-	$sat2 = $_GET['sat2'];
-	$sat3 = $_GET['sat3'];
-	$sut1 = $_GET['sun1'];
-	$sut2 = $_GET['sun2'];
-	$sut3 = $_GET['sun3'];
-
-	//should be able to replace with config.php
-	$mysqli = new mysqli("mysql.eecs.ku.edu", "djoseph", "f2TUteC4dQRqL7jR", "djoseph");
-	if ($mysqli->connect_errno)
+	for($x = 0; $x < $counter; $x++)
 	{
-	  printf("Connect failed: %s\n", $mysqli->connect_error);
-	  exit();
+		$task = $_POST["tasks$taskCounter"];
+		$priority = $_POST["priority$taskCounter"];
+		$date = $_POST["date$taskCounter"];
+		
+		$mysqli = new mysqli("mysql.eecs.ku.edu", "djoseph", "f2TUteC4dQRqL7jR", "djoseph");
+		if ($mysqli->connect_errno)
+		{
+		  printf("Connect failed: %s\n", $mysqli->connect_error);
+		  exit();
+		}
+
+		//searches throught the user name to find the username given
+		$search = "SELECT User FROM projUsers WHERE User = '$username'";
+
+		$search2 = "SELECT title FROM projUsers WHERE title = '$title'";
+
+		//this query adds information to the projData column of the tasks that the user enters into the form
+		$query1 = "INSERT INTO projData (username, title, Task, Priority, Date)
+		 VALUES ('$username','$title','$task','$priority','$date')";
+
+		if(!isset($username) || $username !== '')
+		{
+			if(!isset($title) || $title !== '')
+			{
+				$listOfUsers =  $mysqli->query($search);
+				if($listOfUsers->num_rows>0)
+				{
+				  while($row = $listOfUsers->fetch_assoc())
+				  {
+					if($row["User"] == $username)
+					{
+					  $result = true;
+					  break;
+					}
+				  }
+				}
+
+				if($result)
+				{
+				  if(($mysqli->query($query1)))
+				  {
+					echo PHP_EOL;
+					echo "list successfully added".PHP_EOL;
+				  }
+				  else
+				  {
+						echo PHP_EOL;
+						echo "list not successfully added".PHP_EOL;
+				  }
+				}
+				else
+				{
+					echo PHP_EOL;
+					echo "not successfully added b/c username does not exist".PHP_EOL;
+				}
+			}
+			else
+			{
+				echo PHP_EOL;
+				echo "not successfully added b/c title is empty".PHP_EOL;
+			}
+		}
+		else
+		{
+			echo PHP_EOL;
+			echo "not successfully added b/c not logged in".PHP_EOL;
+		}
+
+		//This function sets up a new file that is going to overwrite things if the file already exists.
+		//$myfileWrite = fopen($x, "w") or die("Can't open file!");
+		//These functions write to the file for the list
+		echo PHP_EOL;
+		$txt = "Title:".$title.PHP_EOL;
+		echo $txt.PHP_EOL;
+		$txt = "<br>"."Task:".$task.PHP_EOL;
+		echo $txt.PHP_EOL;
+		$txt = "<br>"."Priority:".$priority.PHP_EOL;
+		echo $txt.PHP_EOL;
+		$txt = "<br>"."Date:".$date.PHP_EOL;
+		echo $txt.PHP_EOL;
+		$taskCounter++;
+	
 	}
-
-	//searches throught the user name to find the username given
-	$search = "SELECT User FROM projUsers WHERE User = '$username'";
-
-	$search2 = "SELECT title FROM projUsers WHERE title = '$title'";
-
-	//this query adds information to the projData column of the tasks that the user enters into the form
-	$query1 = "INSERT INTO projData (username, title, mondayTask1, mondayTask2, mondayTask3, tuesdayTask1, tuesdayTask2, tuesdayTask3, wednesdayTask1, wednesdayTask2, wednesdayTask3, thursdayTask1, thursdayTask2, thursdayTask3, fridayTask1, fridayTask2, fridayTask3, saturdayTask1, saturdayTask2, saturdayTask3, sundayTask1, sundayTask2, sundayTask3)
-	 VALUES ('$username','$title','$mt1','$mt2','$mt3','$tut1','$tut2','$tut3','$wt1','$wt2','$wt3','$tht1','$tht2','$tht3','$ft1','$ft2','$ft3','$sat1','$sat2','$sat3','$sut1','$sut2','$sut3')";
-
-	//this query selects all of the information from the title of the list in the database that matches the input of the form
-	//$query2 = "SELECT * FROM projData WHERE title == '$title'";
-
-	if(!isset($username) || $username !== '')
-	{
-	    if(!isset($title) || $title !== '')
-	    {
-	     	if($mysqli->query($search2) == false)
-	     	{
-	     	$listOfUsers =  $mysqli->query($search);
-		    if($listOfUsers->num_rows>0)
-		    {
-		      while($row = $listOfUsers->fetch_assoc())
-		      {
-		        if($row["User"] == $username)
-		        {
-		          $result = true;
-		          break;
-		        }
-		      }
-		    }
-
-		    if($result)
-		    {
-		      if(($result = $mysqli->query($query1)) == true)
-		      {
-		        echo "list successfully added".PHP_EOL;
-		      }
-		      else
-		      {
-		        echo "list not successfully added".PHP_EOL;
-		      }
-		    }
-		    else
-		    {
-		      echo "not successfully added b/c username does not exist".PHP_EOL;
-		    }
-	     	}
-	     	else
-	     	{
-                    echo "not successfully added b/c that list already exists".PHP_EOL;
-	     	}
-	    }
-	    else
-	    {
-	    	echo "not successfully added b/c title is empty".PHP_EOL;
-	    }
-	}
-	else
-	{
-	    echo "not successfully added b/c not logged in".PHP_EOL;
-	}
-
-	//This function sets up a new file that is going to overwrite things if the file already exists.
-	//$myfileWrite = fopen($x, "w") or die("Can't open file!");
-	//These functions write to the file for the list
-	$txt = "Title:".$title.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Monday task 1:".$mt1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Monday task 2:".$mt2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Monday task 3:".$mt3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Tuesday task 1:".$tut1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Tuesday task 2:".$tut2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Tuesday task 3:".$tut3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Wednesday task 1:".$wt1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//write($myfileWrite, $txt);
-	$txt = "<br>"."Wednesday task 2:".$wt2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Wednesday task 3:".$wt3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Thursday task 1:".$tht1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Thursday task 2:".$tht2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Thursday task 3:".$tht3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Friday task 1:".$ft1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Friday task 2:".$ft2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Friday task 3:".$ft3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Saturday task 1:".$sat1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Saturday task 2:".$sat2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Saturday task 3:".$sat3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Sunday task 1:".$sut1.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Sunday task 2:".$sut2.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	$txt = "<br>"."Sunday task 3:".$sut3.PHP_EOL;
-	echo $txt.PHP_EOL;
-	//fwrite($myfileWrite, $txt);
-	//fclose($myfileWrite);
-
 	echo "<br>"." ".PHP_EOL;
 	echo "<br>"."Your List was Created!";
 	echo "<br>"."<a href='logout.php'>".'Click to logout.'."</a>";
--- update.php
+++ update.php
@@ -1,3 +1,48 @@
+<html>
+<head>
+ 		<style>
+            #section1
+			{
+				padding:5px;
+				line-height:10px;
+				height:20px;
+				font-size:20;
+			}
+
+			#section2
+			{
+				padding:10px;
+				line-height:10px;
+				font-size:20;
+			}
+
+			.textform1
+			{
+				width:200px;
+			    clear:left;
+			    text-align:right;
+			    padding-right:10px;
+			    float:right;
+			}
+
+			.button1
+			{
+				width:210px;
+			}
+        </style>
+		<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="createlist.js"></script>
+	</head>
+	<body>
+<a href = "createlist.html"><input align="center" type="button" value="Click to return to make a new list"></a>
+	<br><br><a href = "logout.php"><input align="center" type="button" value="Click to logout"></a></br></br>
+	<br></br>
+<form action="edit.php">
 <?php
 
 //Created by Amber Yeasin
@@ -6,222 +51,46 @@ include('config.php');
 
 //local variables
 $username = $_SESSION['login_user'];
-$title = $_GET['Title'];
-$m1 = $_GET['mon1'];
-$m2 = $_GET['mon2'];
-$m3 = $_GET['mon3'];
-$tu1 = $_GET['tue1'];
-$tu2 = $_GET['tue2'];
-$tu3 = $_GET['tue3'];
-$w1 = $_GET['wed1'];
-$w2 = $_GET['wed2'];
-$w3 = $_GET['wed3'];
-$th1 = $_GET['thu1'];
-$th2 = $_GET['thu2'];
-$th3 = $_GET['thu3'];
-$f1 = $_GET['fri1'];
-$f2 = $_GET['fri2'];
-$f3 = $_GET['fri3'];
-$sa1 = $_GET['sat1'];
-$sa2 = $_GET['sat2'];
-$sa3 = $_GET['sat3'];
-$su1 = $_GET['sun1'];
-$su2 = $_GET['sun2'];
-$su3 = $_GET['sun3'];
+$title = $_GET['projData'];
 
+$mysqli = new mysqli("mysql.eecs.ku.edu", "djoseph", "f2TUteC4dQRqL7jR", "djoseph");
+	if ($mysqli->connect_errno)
+	{
+	  printf("Connect failed: %s\n", $mysqli->connect_error);
+	  exit();
+	}
 
-//checks that everything is successfully edited
-if($m1 != ""){
-$um1 = $mysqli->query("UPDATE projData SET mondayTask1 = '$m1' WHERE title = '$title'");
-  if($um1){
-    echo "Monday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Monday task 1"."<br>";
-  }
-}
-if($m2 != ""){
-$um2 = $mysqli->query("UPDATE projData SET mondayTask2 = '$m2' WHERE title = '$title'");
-  if($um2){
-    echo "Monday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Monday task 2"."<br>";
-  }
-}
-if($m3 != ""){
-$um3 = $mysqli->query("UPDATE projData SET mondayTask3 = '$m3' WHERE title = '$title'");
-  if($um3){
-    echo "Monday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Monday task 3"."<br>";
-  }
-}
-if($tu1 != ""){
-$utu1 = $mysqli->query("UPDATE projData SET tuesdayTask1 = '$tu1' WHERE title = '$title'");
-  if($utu1){
-    echo "Tuesday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Tuesday task 1"."<br>";
-  }
-}
-if($tu2 != ""){
-$utu2 = $mysqli->query("UPDATE projData SET tuesdayTask2 = '$tu2' WHERE title = '$title'");
-  if($utu2){
-    echo "Tuesday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Tuesday task 2"."<br>";
-  }
-}
-if($tu3 != ""){
-$utu3 = $mysqli->query("UPDATE projData SET tuesdayTask3 = '$tu3' WHERE title = '$title'");
-  if($utu3){
-    echo "Tuesday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Tuesday task 3"."<br>";
-  }
-}
-if($w1 != ""){
-$uw1 = $mysqli->query("UPDATE projData SET wednesdayTask1 = '$w1' WHERE title = '$title'");
-  if($uw1){
-    echo "Wednesday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Wednesday task 1"."<br>";
-  }
-}
-if($w2 != ""){
-$uw2 = $mysqli->query("UPDATE projData SET wednesdayTask2 = '$w2' WHERE title = '$title'");
-  if($uw2){
-    echo "Wednesday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Wednesday task 2"."<br>";
-  }
-}
-if($w3 != ""){
-$uw3 = $mysqli->query("UPDATE projData SET wednesdayTask3 = '$w3' WHERE title = '$title'");
-  if($uw3){
-    echo "Wednesday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Wednesday task 3"."<br>";
-  }
-}
-if($th1 != ""){
-$uth1 = $mysqli->query("UPDATE projData SET thursdayTask1 = '$th1' WHERE title = '$title'");
-  if($uth1){
-    echo "Thursday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Thursday task 1"."<br>";
-  }
-}
-if($th2 != ""){
-$uth2 = $mysqli->query("UPDATE projData SET thursdayTask2 = '$th2' WHERE title = '$title'");
-  if($uth2){
-    echo "Thursday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Thursday task 2"."<br>";
-  }
-}
-if($th3 != ""){
-$uth3 = $mysqli->query("UPDATE projData SET thursdayTask3 = '$th3' WHERE title = '$title'");
-  if($uth3){
-    echo "Thursday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Thursday task 3"."<br>";
-  }
-}
-if($f1 != ""){
-$uf1 = $mysqli->query("UPDATE projData SET fridayTask1 = '$f1' WHERE title = '$title'");
-  if($uf1){
-    echo "Friday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Friday task 1"."<br>";
-  }
-}
-if($f2 != ""){
-$uf2 = $mysqli->query("UPDATE projData SET fridayTask2 = '$f2' WHERE title = '$title'");
-  if($uf2){
-    echo "Friday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Friday task 2"."<br>";
-  }
-}
-if($f3 != ""){
-$uf3 = $mysqli->query("UPDATE projData SET fridayTask3 = '$f3' WHERE title = '$title'");
-  if($uf3){
-    echo "Friday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Friday task 3"."<br>";
-  }
-}
-if($sa1 != ""){
-$usa1 = $mysqli->query("UPDATE projData SET saturdayTask1 = '$sa1' WHERE title = '$title'");
-  if($usa1){
-    echo "Saturday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Saturday task 1"."<br>";
-  }
-}
-if($sa2 != ""){
-$usa2 = $mysqli->query("UPDATE projData SET saturdayTask2 = '$sa2' WHERE title = '$title'");
-  if($usa2){
-    echo "Saturday task 2 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Saturday task 2"."<br>";
-  }
-}
-if($sa3 != ""){
-$usa3 = $mysqli->query("UPDATE projData SET saturdayTask3 = '$sa3' WHERE title = '$title'");
-  if($usa3){
-    echo "Saturday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Saturday task 3"."<br>";
-  }
-}
-if($su1 != ""){
-$usu1 = $mysqli->query("UPDATE projData SET sundayTask1 = '$su1' WHERE title = '$title'");
-  if($usu1){
-    echo "Sunday task 1 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Sunday task 1"."<br>";
-  }
-}
-if($su2 != ""){
-$usu2 = $mysqli->query("UPDATE projData SET sundayTask2 = '$su2' WHERE title = '$title'");
-if($usu2){
-  echo "Sunday task 2 successfully edited"."<br>";
-}
-else{
-  echo "Unable to edit Sunday task 2"."<br>";
-}
-}
-if($su3 != ""){
-$usu3 = $mysqli->query("UPDATE projData SET sundayTask3 = '$su3' WHERE title = '$title'");
-  if($usu3){
-    echo "Sunday task 3 successfully edited"."<br>";
-  }
-  else{
-    echo "Unable to edit Sunday task 3"."<br>";
-  }
-}
-$mysqli->close();
+$search = "SELECT * FROM projData WHERE title = '$title'";
+echo "<br>"."$title"."</br>".PHP_EOL;
 
-echo "<br>"."<a href='logout.php'>".'Click to logout.'."</a>";
-echo "<br>"."<a href='userLists.php'>".'Click to edit your lists.'."</a>";
+echo "<table border='5'>";
+
+$listsFromUser =  $mysqli->query($search);
+$counter = 0;
+			echo "<tr>".PHP_EOL;
+			echo "<td>"."Task"."</td>".PHP_EOL;
+			echo "<td>"."Priority"."</td>".PHP_EOL;
+			echo "<td>"."Date"."</td>".PHP_EOL;
+			echo "<td>"."Edit Priority"."</td>".PHP_EOL;
+			echo "<tr>".PHP_EOL;
+			//$listsFromUser = $listsFromUser->sort_by("Priority");
+		while($row = $listsFromUser->fetch_assoc())
+		{
+			echo "<tr>".PHP_EOL;
+			echo "<td>".$row["Task"]."</td>".PHP_EOL;
+			echo "<td>".$row["Priority"]."</td>".PHP_EOL;
+			echo "<td>".$row["Date"]."</td>".PHP_EOL;
+			echo "<td>"."Priority"."<select name='priority$counter'>"."<option value='1'>".'1'."</option>"."<option value='2'>".'2'."</option>"."<option value='3'>".'3'."</option>"."
+               <option value='4'>".'4'."</option>"."<option value='5'>".'5'."</option>"."</select>"."</td>".PHP_EOL;
+			echo "</tr>".PHP_EOL;
+			$counter++;
+		}
+		echo "</table>";
+		echo "<input type='hidden' name='counter' value='$counter'>".PHP_EOL;
+		echo "<input type='hidden' name='title' value='$title'>".PHP_EOL;
+
 ?>
+<input type="submit" value="submit">
+</form>
+</body>
+</html>
